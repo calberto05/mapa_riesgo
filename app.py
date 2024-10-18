ab90c0223c231860c8ae9a3912e9a0460308f4b1
calberto05 <carlosalfonsoalberto05@gmail.com>
2024-10-18 15:49:38 -0600

    Creacion Lineas

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, jsonify, render_template
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+MAPS_API_KEY = os.getenv('MAPS_API_KEY')
+
+data = pd.read_csv('data/coordenadas_trafico.csv')
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        return jsonify(request.json)
-    return render_template('index.html')
+    global data
 
-if __name__ == '__main__':
+    coords = [
+        {"lat": data["x1"][0], "lng": data["y1"][0]},  # Coordenada válida
+        {"lat": data["x2"][0], "lng": data["y2"][0]}   # Coordenada válida
+    ]
+        
+    return render_template('index.html', MAPS_API_KEY=MAPS_API_KEY, coords=coords)
+
+if __name__ == '__main__':  
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,45 @@
-// This example creates a 2-pixel-wide red polyline showing the path of
-// the first trans-Pacific flight between Oakland, CA, and Brisbane,
-// Australia which was made by Charles Kingsford Smith.
+// This example adds an animated symbol to a polyline.
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: { lat: 0, lng: -180 },
-      mapTypeId: "terrain",
-    });
-    const flightPlanCoordinates = [
-      { lat: 37.772, lng: -122.214 },
-      { lat: 21.291, lng: -157.821 },
-      { lat: -18.142, lng: 178.431 },
-      { lat: -27.467, lng: 153.027 },
-    ];
-    const flightPath = new google.maps.Polyline({
-      path: flightPlanCoordinates,
-      geodesic: true,
-      strokeColor: "#FF0000",
-      strokeOpacity: 1.0,
-      strokeWeight: 2,
-    });
-  
-    flightPath.setMap(map);
-  }
-  
-  window.initMap = initMap;
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 20.291, lng: 153.027 },
+    zoom: 6,
+    mapTypeId: "terrain",
+  });
+
+  const lineSymbol = {
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 8,
+    strokeColor: "#393",
+  };
+
+  const line = new google.maps.Polyline({
+    path: [
+      { lat: 19.4037031532808, lng: -99.1692699611328 },
+      { lat: 19.4229921001133, lng: -99.163152275508 },
+    ],
+    icons: [
+      {
+        icon: lineSymbol,
+        offset: "100%",
+      },
+    ],
+    map: map,
+  });
+
+  animateCircle(line);
+}
+
+function animateCircle(line) {
+  let count = 0;
+
+  window.setInterval(() => {
+    count = (count + 1) % 200;
+
+    const icons = line.get("icons");
+
+    icons[0].offset = count / 2 + "%";
+    line.set("icons", icons);
+  }, 20);
+}
+
+export { initMap };
