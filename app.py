fc01e04d38614d3d5aed481f6545b68760c12f19
calberto05 <carlosalfonsoalberto05@gmail.com>
2024-10-20 18:06:36 -0600

    Demo Con velocidades aleatorias

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import random
 
 load_dotenv()
 MAPS_API_KEY = os.getenv('MAPS_API_KEY')
@@ -13,13 +14,15 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global data
-
-    coords = [
-        {"lat": data["x1"][0], "lng": data["y1"][0]},  # Coordenada válida
-        {"lat": data["x2"][0], "lng": data["y2"][0]}   # Coordenada válida
-    ]
         
-    return render_template('index.html', MAPS_API_KEY=MAPS_API_KEY, coords=coords)
+    coords = []
+    velocidades = []
+    for i in range(len(data)):
+        coords.append([{"lat": data['x1'][i], "lng": data['y1'][i]}, {"lat": data['x2'][i], "lng": data['y2'][i]}])
+        # generar aleatoriamente velocidades entre 0 y 2 float
+        velocidades.append(random.uniform(0, 2))
+
+    return render_template('index.html', MAPS_API_KEY=MAPS_API_KEY, coords=coords, velocidades=velocidades)
 
 if __name__ == '__main__':  
     app.run(debug=True)
